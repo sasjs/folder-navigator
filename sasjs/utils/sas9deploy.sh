38baa58707dd2dfa07b769d0716390e47dc172a5
Mihajlo <mihajlo.medjedovic@outlook.com>
2021-03-16 16:21:37 +0100

    chore: changes

--- sasjs/utils/sas9deploy.sh
+++ sasjs/utils/sas9deploy.sh
@@ -2,6 +2,6 @@
 
 # copy the build file to the SAS server where it can be %inc'd as part of an STP
 
-rsync -avhe ssh sasjsbuild/sas9deploy.sas --delete mihmed@sas.analytium.co.uk:/tmp
+rsync -avhe ssh sasjsbuild/sas9.sas --delete mihmed@sas.analytium.co.uk:/tmp
 
-echo "Now run: %inc '/tmp/sas9deploy.sas';"
+echo "Now run: %inc '/tmp/sas9.sas';"
